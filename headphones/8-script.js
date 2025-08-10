c0632e3486964a8102989bc823be50df5361af7d
alikagwe <alikagwe@yahoo.com>
2025-08-10 03:02:15 +0300

    Hambugher added

---
+++ headphones/8-script.js
@@ -0,0 +1,17 @@
+
+const hamburger = document.getElementById('hamburger');
+const navMenu = document.getElementById('nav-menu');
+
+
+hamburger.addEventListener('click', () => {
+    hamburger.classList.toggle('active');
+    navMenu.classList.toggle('show');
+});
+
+
+document.querySelectorAll('#nav-menu a').forEach(link => {
+    link.addEventListener('click', () => {
+        hamburger.classList.remove('active');
+        navMenu.classList.remove('show');
+    });
+});
